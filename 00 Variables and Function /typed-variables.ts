8e1482ce17b5697d22e5799bfef812066402f3d4
Umang Hirani <umanghirani.exe@gmail.com>
2026-01-01 22:54:26 +0530

    typed-variables

---
+++ 00 Variables and Function /typed-variables.ts
@@ -0,0 +1,10 @@
+// PRIMITIVE TYPES
+
+let n: number = 4; // this is how we type variables in typescript
+n = 5; // OK
+n = "asdf"; // Error !
+
+let s: string; // typing before assignment
+s = "asdf";
+s = "asdf"; // OK
+s = 5; // Error !
